ead95e720c0cf2a85dff6115744a385d436c406b
Matt Ericksen <ericksenm93@ymail.com>
2020-07-29 23:58:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -141,7 +141,15 @@ def num_points_scored(player_name)
   end
 end
 def shoe_size(player_name)
-  game_hash
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players
+        data.each do |d_i|
+          return d_i[:points] if d_i[:player_name] == player_name
+        end
+      end
+    end
+  end
 end
 def team_colors(team_name)
   game_hash
