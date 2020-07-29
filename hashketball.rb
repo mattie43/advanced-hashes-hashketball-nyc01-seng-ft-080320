96cd4ef05ab15ce4aef18469d73cab6daa3b25d1
Matt Ericksen <ericksenm93@ymail.com>
2020-07-29 23:41:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,7 +132,9 @@ end
 def num_points_scored(player_name)
   binding.pry
   game_hash.each do |location, team_data|
+    binding.pry
     team_data.each do |attribute, data|
+      binding.pry
       data.each do |data_item|
         binding.pry
       end
