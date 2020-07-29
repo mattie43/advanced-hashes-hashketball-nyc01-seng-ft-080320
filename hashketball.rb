9c86680fa4ba62e4581ad3fec6dfeb973294d2ab
Matt Ericksen <ericksenm93@ymail.com>
2020-07-29 23:34:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,7 +128,13 @@ end
 
 # Write code here
 def num_points_scored(player_name)
-  game_hash
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      data.each do |data_item|
+        #code
+      end
+    end
+  end
 end
 def shoe_size(player_name)
   game_hash
