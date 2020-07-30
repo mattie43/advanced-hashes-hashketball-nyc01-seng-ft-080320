5314cacc50e03c3b909e77ef25203b92ea52af7f
Matt Ericksen <ericksenm93@ymail.com>
2020-07-30 00:29:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -165,7 +165,7 @@ end
 def player_stats(player_name)
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
-      binding.pry
+      return data if data[:player_name] == player_name
     end
   end
 end
